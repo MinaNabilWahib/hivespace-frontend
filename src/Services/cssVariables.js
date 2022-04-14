73c4c2329e09fd4ffe7feccbaa96be0ed57f6057
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-04-14 22:43:27 +0200

    update CSS variables and add theme

--- src/Services/cssVariables.js
+++ src/Services/cssVariables.js
@@ -1,16 +1,18 @@
-
+// import './cssVariables.css';
 const cssVariables = {
     colors: (alpha = 1) => ({
-        primaryColor: `rgba(47, 47, 162,${alpha})`,
-        primaryColorDark: `rgba(36, 37, 130,${alpha})`,
-        primaryColorLight: `rgba(101, 157, 189,${alpha})`,
-        secondaryColor: `rgba(246, 76, 114,${alpha})`,
-        secondaryColorDark: `rgba(85, 61, 103,${alpha})`,
-        secondaryColorLight: `rgba(153, 115, 142,${alpha})`,
-        whiteColor: `rgba(255, 255, 255,${alpha})`,
-        grayColor: `rgba(77, 77, 78,${alpha})`,
-        darkColor: `rgba(31, 41, 55,${alpha})`,
-        darkColor2: `rgb(42, 48, 60,${alpha})`,
+        primaryColor: `rgba(var(--primary-color),${alpha})`,
+        primaryColorDark: `rgba(var(--primary-color_dark),${alpha})`,
+        primaryColorLight: `rgba(var(--primary-color_light),${alpha})`,
+        secondaryColor: `rgba(var(--secondary-color),${alpha})`,
+        secondaryColorDark: `rgba(var(--secondary-color_dark),${alpha})`,
+        secondaryColorLight: `rgba(var(--secondary-color_light),${alpha})`,
+        whiteColor: `rgba(var(--color-white),${alpha})`,
+        grayColor: `rgba(var(--color-gray),${alpha})`,
+        darkColor: `rgba(var(--dark-color),${alpha})`,
+        bgColor: `rgba(var(--color-background),${alpha})`,
+        txtColor: `rgba(var(--colors-text),${alpha})`,
+        headingColor: `var(--colors-heading)`,
     }),
     transitions: {
         transitionOut: 'all 0.4s cubic-bezier(0.6, -0.28, 0.735, 0.045)',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				primary: 'rgb(var(--primary-color))',
+				'primary-dark': 'rgb(var(--primary-color_dark))',
+				'primary-light': 'rgb(var(--primary-color_light))',
+				secondary: 'rgb(var(--secondary-color))',
+				'secondary-dark': 'rgb(var(--secondary-color_dark))',
+				'secondary-light': 'rgb(var(--secondary-color_light))',
+				dark: 'rgb(var(--color-gray))',
+				gry: 'rgb(var(--color-gray))',
+				main: 'rgb(var(--color-background))',
+				'main-heading': 'var(--colors-heading)',
+			},
+			textColor: {
+				primary: 'rgb(var(--primary-color))',
+				'primary-dark': 'rgb(var(--primary-color_dark))',
+				'primary-light': 'rgb(var(--primary-color_light))',
+				secondary: 'rgb(var(--secondary-color))',
+				'secondary-dark': 'rgb(var(--secondary-color_dark))',
+				'secondary-light': 'rgb(var(--secondary-color_light))',
+				dark: 'rgb(var(--color-gray))',
+				gry: 'rgb(var(--color-gray))',
+				main: 'rgb(var(--color-text))',
+			}
+		},
 	},
 	plugins: [],
-};
+}
