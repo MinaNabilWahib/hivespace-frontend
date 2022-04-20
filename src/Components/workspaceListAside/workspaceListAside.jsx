28894b28a1e39dd47f419fe3547c56016b0d0836
yasserAbdelkader98 <yasser.abdelkader2020@gmail.com>
2022-04-21 00:34:12 +0200

    adding new workspace

--- src/Components/workspaceListAside/workspaceListAside.jsx
+++ src/Components/workspaceListAside/workspaceListAside.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { setOpenWorkspace } from "../../Store/actions/workspace";
 import WorkspaceListIcon from "./workspaceListIcon";
+import Addworkspace from "./addWorkspace";
 
 const WorkspaceListAside = ({ workspaces, openWorkspace }) => {
   const dispatch = useDispatch();
@@ -17,26 +18,8 @@ const WorkspaceListAside = ({ workspaces, openWorkspace }) => {
         );
       })}
       {/* TODO:: ADD WORKSPACE */}
-      <a
-        className="flex items-center justify-center w-10 h-10 rounded-lg bg-transparent hover:bg-gray-400"
-        href="#"
-        onClick={() => console.log("ADD WORKSPACE")}
-      >
-        <svg
-          className="w-6 h-6 fill-current"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          ></path>
-        </svg>
-      </a>
+
+      <Addworkspace />
     </div>
   );
 };
--- src/Pages/Workspace/workspacePage.js
+++ src/Pages/Workspace/workspacePage.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ChannelLoop from "../../Components/channelComponent/channelLoop";
 import Chat from "../../Components/ChatComponent/chatComponent";
+import Addworkspace from "../../Components/workspaceListAside/addWorkspace";
 import WorkspaceListAside from "../../Components/workspaceListAside/workspaceListAside";
 import WorkspaceSidebar from "../../Components/workspaceSidebar/workspaceSidebar";
 
