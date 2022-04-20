7ffec6516f8df2679b87e1ac950c3e3b72143305
yasserAbdelkader98 <yasser.abdelkader2020@gmail.com>
2022-04-21 00:44:58 +0200

    adding workspace after merging

--- src/Pages/Workspace/workspacePage.js
+++ src/Pages/Workspace/workspacePage.js
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ChannelLoop from "../../Components/channelComponent/channelLoop";
 import Chat from "../../Components/ChatComponent/chatComponent";
 import Addworkspace from "../../Components/workspaceListAside/addWorkspace";
 import WorkspaceListAside from "../../Components/workspaceListAside/workspaceListAside";
 import WorkspaceSidebar from "../../Components/workspaceSidebar/workspaceSidebar";
 
 const Workspace = () => {
-	return (
-		<div className="flex w-screen h-screen text-gray-700">
-			<WorkspaceListAside />
-			<WorkspaceSidebar />
-			<Chat />
-		</div>
-	);
+  return (
+    <div className="flex w-screen h-screen text-gray-700">
+      <WorkspaceListAside />
+      <WorkspaceSidebar />
+      <Chat />
+    </div>
+  );
 };
 
 export default Workspace;
